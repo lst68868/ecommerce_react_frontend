e38f458d7d56c6f6500c211172a66ae75ea69339
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-06-02 15:36:08 -0400

    removed fluff

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -2,19 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
-
 function NavBar() {
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
         </li>
         <li>
           <Link to="/login">Login</Link>
