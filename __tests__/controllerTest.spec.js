14ad047f530c14f9f56b9d89eb8e7ee2b59c946c
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-06-26 20:15:27 -0400

    adding jest tests failing

---
+++ __tests__/controllerTest.spec.js
@@ -0,0 +1,8 @@
+describe("GIVEN I am adding numbers", () => {
+  test("THEN I expect 2 + 2 to equal 4", () => {
+    const num1 = 2;
+    const num2 = 3;
+    const result = num1 + num2;
+    expect(result).toBe(4);
+  });
+});
